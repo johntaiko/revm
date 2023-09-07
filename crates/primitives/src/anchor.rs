918a31fa11bd2e97a05874866e674f987a66e50f
john xu <john@taiko.xyz>
2023-09-07 10:43:38 +0000

    fix: validate anchor

--- crates/primitives/src/anchor.rs
+++ crates/primitives/src/anchor.rs
@@ -15,11 +15,11 @@ pub static TREASURY: Lazy<B160> = Lazy::new(|| {
 
 pub(crate) fn validate(env: &Env) -> bool {
     !env.is_anchor()
-        || (env.tx.tx_type != TxType::Eip1559
-            && env.tx.transact_to != TransactTo::Call(env.taiko.l2_address)
-            && u32::from_be_bytes(env.tx.data[..4].try_into().unwrap()) != ANCHOR_SELECTOR
-            && env.tx.value != U256::ZERO
-            && env.tx.gas_limit != ANCHOR_GAS_LIMIT
-            && env.tx.gas_price != env.block.basefee
-            && env.tx.caller != *GOLDEN_TOUCH_ACCOUNT)
+        || (env.tx.tx_type == TxType::Eip1559
+            && env.tx.transact_to == TransactTo::Call(env.taiko.l2_address)
+            && u32::from_be_bytes(env.tx.data[..4].try_into().unwrap()) == ANCHOR_SELECTOR
+            && env.tx.value == U256::ZERO
+            && env.tx.gas_limit == ANCHOR_GAS_LIMIT
+            && env.tx.gas_price == env.block.basefee
+            && env.tx.caller == *GOLDEN_TOUCH_ACCOUNT)
 }
